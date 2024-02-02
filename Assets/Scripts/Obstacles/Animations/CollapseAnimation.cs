d3fcc5a816644abc233b250a1d06d5352f4d78b2
streamlineddesigns <streamline.our.designs@gmail.com>
2024-02-01 22:38:29 -0500

    Finishing initial Collapse Double Sided Between obstacles

--- Assets/Scripts/Obstacles/Animations/CollapseAnimation.cs
+++ Assets/Scripts/Obstacles/Animations/CollapseAnimation.cs
@@ -41,7 +41,7 @@ namespace StudioByStorm.Obstacles.Animations {
 
                 yield return StartCoroutine(Move(buildingBlockPositions, collapseTime));
 
-                yield return new WaitForSeconds(2.0f);
+                yield return new WaitForSeconds(4.0f);
             }
         }
 

2a47599121fade07f2cf95a5097c3d84e360d660
streamlineddesigns <streamline.our.designs@gmail.com>
2024-09-14 21:57:41 -0400

    Update AnalyticsManager.cs

--- Assets/Scripts/AnalyticsManager.cs
+++ Assets/Scripts/AnalyticsManager.cs
@@ -17,13 +17,17 @@ namespace StudioByStorm {
 
         public static void NewProgressionEvent(GAProgressionStatus status, int chapter, int level)
         {
-            if (isDebugging) Debug.Log("Status: " + status.ToString() + " Chapter: " + chapter.ToString()  + " Level: " + level.ToString());
+            if (isDebugging) {
+                Debug.Log("Status: " + status.ToString() + " Chapter: " + chapter.ToString()  + " Level: " + level.ToString());
+            }
             GameAnalytics.NewProgressionEvent(status, chapter.ToString(), level.ToString());
         }
 
         public static void NewProgressionEvent(GAProgressionStatus status, int chapter, int level, int score)
         {
-            if (isDebugging) Debug.Log("Status: " + status.ToString() + " Chapter: " + chapter.ToString()  + " Level: " + level.ToString()  + " Score: " + score.ToString());
+            if (isDebugging) {
+                Debug.Log("Status: " + status.ToString() + " Chapter: " + chapter.ToString()  + " Level: " + level.ToString()  + " Score: " + score.ToString());
+            }
             GameAnalytics.NewProgressionEvent(status, chapter.ToString(), level.ToString(), score.ToString());
         }
     }

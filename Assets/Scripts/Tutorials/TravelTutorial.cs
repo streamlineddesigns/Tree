ac12492225446479f20cca91b4aac20bcaf21580
streamlineddesigns <streamline.our.designs@gmail.com>
2025-11-20 20:42:39 -0500

    Revert "Bug fixes ie tutorials"
    This reverts commit 0441ca3a6dd7364acd7d95efcfb4d1a10894049d.

--- Assets/Scripts/Tutorials/TravelTutorial.cs
+++ Assets/Scripts/Tutorials/TravelTutorial.cs
@@ -7,7 +7,6 @@ using DG.Tweening;
 using StudioByStorm.EventPublishers;
 using StudioByStorm.UI.Controllers;
 using StudioByStorm.ML.Clustering;
-using StudioByStorm.Optimizations;
 
 namespace StudioByStorm.Tutorials {
 
@@ -27,7 +26,6 @@ namespace StudioByStorm.Tutorials {
         private ActionController actionController;
         private bool isJumpLocked;
         private Edge secondaryEdge;
-        private int targetNodeID;
 
         protected void OnEnable()
         {
@@ -114,8 +112,6 @@ namespace StudioByStorm.Tutorials {
             
             //the animation steps
             if (targetNode != null) {
-                targetNodeID = targetNode.ID;
-
                 actionController.GetEdgeButtonClick();
 
                 Edge currentEdge = actionController.ActionModel.CurrentEdge;
@@ -163,7 +159,6 @@ namespace StudioByStorm.Tutorials {
                     GameObject startGO = GameManager.Singleton.NodeRegistry.TryGetValue(nodeID).gameObject;
                     GameObject endGO = GameManager.Singleton.NodeRegistry.TryGetValue(startNodeIDToEndNodeIDForTutorials[nodeID]).gameObject;
                     GameManager.Singleton.FXManager.fingerSlingShotAnimation.SetPositions(startGO, endGO);
-
                     if (!GameManager.Singleton.FXManager.fingerSlingShotAnimation.isAnimating) {
                         GameManager.Singleton.FXManager.fingerSlingShotAnimation.gameObject.SetActive(true);
                         GameManager.Singleton.FXManager.fingerSlingShotAnimation.Animate();
@@ -171,27 +166,6 @@ namespace StudioByStorm.Tutorials {
                 }
 
                 yield return new WaitForSeconds(0.0333f);
-            }
-        }
-
-        protected void Update()
-        {
-            if (Input.GetMouseButton(0)) {
-                Vector2 clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                Vector2 cellID = GameManager.Singleton.SpatialHashManager.SpatialHash.GetCellIDForPos(clickPos);
-                List<HashData> nearbyNodes = GameManager.Singleton.SpatialHashManager.SpatialHash.GetNearby(cellID);
-                List<HashData> nearestNeighbor = KNN.GetKNearestNeighbors(clickPos, nearbyNodes, 1);
-                HashData nearbyNode = (nearestNeighbor.Count > 0 && nearestNeighbor[0] != null) ? nearestNeighbor[0] : null;
-                Node selectedNode = nearbyNode.GetData<Node>();
-                bool isNearbyNodeWithinClickDistance = (nearbyNode != null && selectedNode != null && selectedNode.ID == targetNodeID && ML.Math.GetDistance(nearbyNode.gameObject.transform.position, clickPos) <= 3.0f);
-
-                if (isNearbyNodeWithinClickDistance) {
-                    isJumpLocked = false;
-                    actionController.LockJump(isJumpLocked);
-                } else {
-                    isJumpLocked = true;
-                    actionController.LockJump(isJumpLocked);
-                }
             }
         }
 

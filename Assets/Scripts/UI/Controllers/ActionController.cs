973f45d6e5cc816738799f3497f54aeb4f1a6ca2
streamlineddesigns <streamline.our.designs@gmail.com>
2025-11-26 02:20:51 -0500

    Abstract cell

--- Assets/Scripts/UI/Controllers/ActionController.cs
+++ Assets/Scripts/UI/Controllers/ActionController.cs
@@ -609,7 +609,7 @@ namespace StudioByStorm {
                 GetEdgeButtonClick();
             } else {
                 GameManager.Singleton.LevelManager.parentColorsConnected[ActionModel.CurrentNode.NodeColor] = true;
-                Node[] nodes = GameManager.Singleton.ColorNodeRegistry.TryGetValue(ActionModel.CurrentNode.NodeColor).Where(x => x.NodeType != NodeType.Parent/* && x.isNodeCellular*/).ToArray();
+                Node[] nodes = GameManager.Singleton.ColorNodeRegistry.TryGetValue(ActionModel.CurrentNode.NodeColor).Where(x => x.NodeType != NodeType.Parent && x.isNodeCellular).ToArray();
                 for (int i = 0; i < nodes.Length; i++) {
                     nodes[i].AddColorRing(true);
                 }

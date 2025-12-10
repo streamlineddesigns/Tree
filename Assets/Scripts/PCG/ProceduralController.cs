16dd7a3f4cc5488032aa1b75844a68d9770a4cb2
streamlineddesigns <streamline.our.designs@gmail.com>
2025-12-09 23:02:04 -0500

    bug fix ie pcg

--- Assets/Scripts/PCG/ProceduralController.cs
+++ Assets/Scripts/PCG/ProceduralController.cs
@@ -33,11 +33,10 @@ namespace StudioByStorm.PCG {
         {
             ProceduralLevelGenerator.DependencyInjection(GraphConstructionManager);
             yield return StartCoroutine(ProceduralLevelGenerator.CreateLevel());
-            yield return new WaitForSeconds(0.1f);
 
             ProceduralObstacleSelector.DependencyInjection(ObstacleDataRepository, GraphConstructionManager, LevelConfig);
             yield return StartCoroutine(ProceduralObstacleSelector.SelectObstacles());
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitUntil(() => GraphConstructionManager.GlobalLevelData.obstacleNames != null);
 
             ProceduralObstaclePlacer.DependencyInjection(ObstacleDataRepository, GraphConstructionManager);
             yield return StartCoroutine(ProceduralObstaclePlacer.PlaceObstacles());

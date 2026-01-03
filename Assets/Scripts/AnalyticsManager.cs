b6577f8602b4829144f0cfd0c3343266db2fdb5d
streamlineddesigns <streamline.our.designs@gmail.com>
2026-01-03 13:47:21 -0500

    Bug fix ie missing level progression dataa issue

--- Assets/Scripts/AnalyticsManager.cs
+++ Assets/Scripts/AnalyticsManager.cs
@@ -18,7 +18,7 @@ namespace StudioByStorm {
             //initialize analytics normally
             if (playerAge >= minAge) {
                 GameAnalytics.Initialize();
-            //initialize analytics without Personally Identifiable Information ie COPPA compliancy
+            //initialize analytics without Personally Identifiable Information ie GDPR & COPPA compliancy
             } else {
                 GameAnalytics.EnableAdvertisingIdTracking(false);
                 GameAnalytics.Initialize();
@@ -27,24 +27,24 @@ namespace StudioByStorm {
 
         public static void NewProgressionEvent(GAProgressionStatus status, LevelPackName lpn, int chapter, int level)
         {
-            if (playerAge >= minAge) {
+            //if (playerAge >= minAge) {
                 string LevelPackChapter = lpn.ToString() + chapter.ToString();
                 if (isDebugging) {
                     Debug.Log("Status: " + status.ToString() + " Level Pack: " + lpn.ToString() + " Chapter: " + chapter.ToString()  + " Level: " + level.ToString());
                 }
                 GameAnalytics.NewProgressionEvent(status, LevelPackChapter, level.ToString());
-            }
+            //}
         }
 
         public static void NewProgressionEvent(GAProgressionStatus status, LevelPackName lpn, int chapter, int level, int score)
         {
-            if (playerAge >= minAge) {
+            //if (playerAge >= minAge) {
                 string LevelPackChapter = lpn.ToString() + chapter.ToString();
                 if (isDebugging) {
                     Debug.Log("Status: " + status.ToString() + " Level Pack: " + lpn.ToString() + " Chapter: " + chapter.ToString()  + " Level: " + level.ToString()  + " Score: " + score.ToString());
                 }
                 GameAnalytics.NewProgressionEvent(status, LevelPackChapter, level.ToString(), score.ToString());
-            }
+            //}
         }
     }
 
